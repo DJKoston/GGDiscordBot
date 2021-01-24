04de7454a18a12de4d5b61b415bff9cd08ddbd80
Haydon Koston <haydonkoston@hotmail.co.uk>
2021-01-24 17:44:52 +0000

    Removed Simpsons Quote
    Reference: Issue #54

--- DiscordBot.Bots/Commands/MiscCommands.cs
+++ DiscordBot.Bots/Commands/MiscCommands.cs
@@ -310,46 +310,6 @@ namespace DiscordBot.Bots.Commands
             await ctx.Channel.SendMessageAsync(embed: embed).ConfigureAwait(false);
         }
 
-        [Command("simpsons")]
-        [Description("Get a Simpsons Quote!")]
-        public async Task SimpsonsQuote(CommandContext ctx)
-        {
-            await ctx.TriggerTypingAsync();
-
-            var request = new HttpClient
-            {
-                BaseAddress = new Uri("http://thesimpsonsquoteapi.glitch.me")
-            };
-
-            request.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-            request.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate");
-            request.DefaultRequestHeaders.Add("Accept-Language", "en-GB,en-US;q=0.9,en;q=0.8");
-            request.DefaultRequestHeaders.Add("Cache-Control", "max-age=0");
-            request.DefaultRequestHeaders.Add("Connection", "keep-alive");
-            request.DefaultRequestHeaders.Add("Upgrade-Insecure-Requests", "1");
-            request.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36");
-
-            HttpResponseMessage response = await request.GetAsync("quotes");
-
-            var resp = await response.Content.ReadAsStringAsync();
-
-            var formatted = resp.Replace("[", string.Empty);
-            var formattedresp = formatted.Replace("]", string.Empty);
-
-            var simpsons = JsonConvert.DeserializeObject<TheSimpsons>(formattedresp);
-
-            var embed = new DiscordEmbedBuilder
-            {
-                Title = $"{simpsons.Quote}",
-                Description = $"{simpsons.Character}",
-                Color = DiscordColor.Yellow,
-            };
-
-            embed.WithImageUrl(simpsons.ImageURL);
-
-            await ctx.Channel.SendMessageAsync(embed: embed).ConfigureAwait(false);
-        }
-
         [Command("advice")]
         public async Task Advice(CommandContext ctx)
         {
