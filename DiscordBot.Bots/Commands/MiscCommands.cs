8b2f40e05b68d897dc9e928789a775230e4dc208
Haydon Koston <haydonkoston@hotmail.co.uk>
2022-01-05 21:07:35 +0000

    Adjusted MiscCommands

--- DiscordBot.Bots/Commands/MiscCommands.cs
+++ DiscordBot.Bots/Commands/MiscCommands.cs
@@ -6,17 +6,15 @@
         private readonly ISuggestionService _suggestionService;
         private readonly ICommunityStreamerService _communityStreamerService;
         private readonly INowLiveStreamerService _nowLiveStreamerService;
-        private readonly IGoodBotBadBotService _goodBotBadBotService;
         private readonly IConfiguration _configuration;
         private readonly ISimpsonsQuoteService _simpsonsQuoteService;
 
-        public MiscCommands(RPGContext context, ISuggestionService suggestionService, ICommunityStreamerService communityStreamerService, INowLiveStreamerService nowLiveStreamerService, IGoodBotBadBotService goodBotBadBotService, IConfiguration configuration, ISimpsonsQuoteService simpsonsQuoteService)
+        public MiscCommands(RPGContext context, ISuggestionService suggestionService, ICommunityStreamerService communityStreamerService, INowLiveStreamerService nowLiveStreamerService, IConfiguration configuration, ISimpsonsQuoteService simpsonsQuoteService)
         {
             _context = context;
             _suggestionService = suggestionService;
             _communityStreamerService = communityStreamerService;
             _nowLiveStreamerService = nowLiveStreamerService;
-            _goodBotBadBotService = goodBotBadBotService;
             _configuration = configuration;
             _simpsonsQuoteService = simpsonsQuoteService;
         }
